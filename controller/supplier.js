089d130fc622884b6daf2c15facf155ff9b32812
Adham Mamdouh <mamdouhadham57@gmail.com>
2023-11-26 17:28:49 +0200

    Operated Supplier CRUD logic

--- controller/supplier.js
+++ controller/supplier.js
@@ -1,11 +1,70 @@
 const Supplier = require('../models/supplier');
 
-exports.createSupplier = async (req, res) => {};
+exports.createSupplier = async (req, res) => {
+    try {
+        const supplier = new Supplier(req.body);
+        await supplier.save();
+        res.status(201).send(supplier);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
 
-exports.getSuppliers = async (req, res) => {};
+exports.getSuppliers = async (req, res) => {
+    try {
+        const suppliers = await Supplier.find();
+        res.send(suppliers);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
-exports.getSupplierById = async (req, res) => {};
+exports.getSupplierById = async (req, res) => {
+    try {
+        const supplier = await Supplier.findById(req.params.id);
+        if (!supplier) {
+            return res.status(404).send();
+        }
+        res.send(supplier);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
-exports.updateSupplierById = async (req, res) => {};
+exports.updateSupplierById = async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'contactInfo'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
-exports.deleteSupplierById = async (req, res) => {};
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const supplier = await Supplier.findById(req.params.id);
+
+        if (!supplier) {
+            return res.status(404).send();
+        }
+
+        updates.forEach((update) => (supplier[update] = req.body[update]));
+        await supplier.save();
+        res.send(supplier);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+exports.deleteSupplierById = async (req, res) => {
+    try {
+        const supplier = await Supplier.findByIdAndDelete(req.params.id);
+
+        if (!supplier) {
+            return res.status(404).send();
+        }
+
+        res.send(supplier);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
