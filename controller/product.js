15dc62fc7c7a0f018619f75b60d998ef98ba7922
Adham Mamdouh <mamdouhadham57@gmail.com>
2023-11-26 17:16:20 +0200

    Created Initial Product Routes

---
+++ controller/product.js
@@ -0,0 +1,11 @@
+const Product = require('../models/product');
+
+exports.createProduct = async (req, res) => {};
+
+exports.getProducts = async (req, res) => {};
+
+exports.getProductById = async (req, res) => {};
+
+exports.updateProductById = async (req, res) => {};
+
+exports.deleteProductById = async (req, res) => {};
