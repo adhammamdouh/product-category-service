207160a6d46eab7e24e1458c8812ef5692e9a77a
Adham Mamdouh <mamdouhadham57@gmail.com>
2023-11-26 17:53:06 +0200

    Moved Product BL to service

--- controller/product.js
+++ controller/product.js
@@ -1,70 +1,49 @@
-const Product = require('../models/product');
+const productService = require('../services/product');
 
 exports.createProduct = async (req, res) => {
-    try {
-        const product = new Product(req.body);
-        await product.save();
-        res.status(201).send(product);
-      } catch (error) {
-        res.status(400).send(error);
-      }
+  try {
+    const product = await productService.createProduct(req.body);
+    res.status(201).send(product);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 };
 
 exports.getProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.send(products);
-      } catch (error) {
-        res.status(500).send(error);
-      }
+  try {
+    const products = await productService.getProducts();
+    res.send(products);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
 exports.getProductById = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) {
-          return res.status(404).send();
-        }
-        res.send(product);
-      } catch (error) {
-        res.status(500).send(error);
-      }
+  try {
+    const product = await productService.getProductById(req.params.id);
+    if (!product) {
+      return res.status(404).send({ error: 'Product not found' });
+    }
+    res.send(product);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
 
 exports.updateProductById = async (req, res) => {
-    const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'description', 'price', 'category', 'variants', 'supplier'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
-
   try {
-    const product = await Product.findById(req.params.id);
-
-    if (!product) {
-      return res.status(404).send();
-    }
-
-    updates.forEach((update) => (product[update] = req.body[update]));
-    await product.save();
+    const product = await productService.updateProductById(req.params.id, req.body);
     res.send(product);
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send({ error: error.message });
   }
 };
 
 exports.deleteProductById = async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
-    
-        if (!product) {
-          return res.status(404).send();
-        }
-    
-        res.send(product);
-      } catch (error) {
-        res.status(500).send(error);
-      }
+  try {
+    const product = await productService.deleteProductById(req.params.id);
+    res.send(product);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 };
