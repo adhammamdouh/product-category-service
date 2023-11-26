77aab983db1efdcb9694a37e227de9e067092683
Adham Mamdouh <mamdouhadham57@gmail.com>
2023-11-26 17:28:29 +0200

    Operated Category CRUD logic

--- controller/category.js
+++ controller/category.js
@@ -1,11 +1,70 @@
 const Category = require('../models/category');
 
-exports.createCategory = async (req, res) => {};
+exports.createCategory = async (req, res) => {
+    try {
+        const category = new Category(req.body);
+        await category.save();
+        res.status(201).send(category);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
 
-exports.getCategories = async (req, res) => {};
+exports.getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.send(categories);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
-exports.getCategoryById = async (req, res) => {};
+exports.getCategoryById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        if (!category) {
+            return res.status(404).send();
+        }
+        res.send(category);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
 
-exports.updateCategoryById = async (req, res) => {};
+exports.updateCategoryById = async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'description'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
-exports.deleteCategoryById = async (req, res) => {};
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const category = await Category.findById(req.params.id);
+
+        if (!category) {
+            return res.status(404).send();
+        }
+
+        updates.forEach((update) => (category[update] = req.body[update]));
+        await category.save();
+        res.send(category);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+exports.deleteCategoryById = async (req, res) => {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id);
+
+        if (!category) {
+            return res.status(404).send();
+        }
+
+        res.send(category);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
